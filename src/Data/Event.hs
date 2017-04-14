7af16f91d51ae708f8a448b202cf94d4a195f3a2
James N. V. Cash <james.nvc@gmail.com>
2017-04-14 12:23:31 -0400

    Make braid events more well-typed

--- src/Data/Event.hs
+++ src/Data/Event.hs
@@ -2,47 +2,110 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
 module Data.Event (
     Event(..)
-  , eventType
-  , eventPayload
+  , _NewUser
+  , _NewAdmin
+  , _UserLeft
+  , _Other
   , readEvent
   , dumpEvent
 ) where
 
 import Prelude
 
+import Control.Applicative ( (<$>), (<*>))
 import Control.Lens
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
+import Data.Map ()
 import qualified Data.MessagePack as MP
 import Data.Serialize (decodeLazy, encode)
 import Data.Transit
+import Data.UUID
 
-data Event = Event {
-    _eventType :: Text
-  , _eventPayload :: [Transit]
-} deriving (Eq, Show)
-makeLenses ''Event
+type UserId = UUID
+type GroupId = UUID
 
-instance Transitable (Maybe Event) where
-  fromTransit (TransitArray ((TransitKeyword eType) : rest)) = Just $ Event eType rest
-  fromTransit _ = Nothing
+data Event = NewUser [GroupId] UserId
+           | NewAdmin GroupId UserId
+           | UserLeft GroupId UserId
+           | Other Transit
+  deriving (Eq, Show)
+makePrisms ''Event
 
-  toTransit (Just Event{..}) = TransitArray ((TransitKeyword _eventType):_eventPayload)
-  toTransit Nothing = TransitNil
+--transitLookup :: Text -> Maybe Transit
+transitLookup :: (Control.Lens.Index m ~ Transit, Applicative f, At m) =>
+                    Text -> (IxValue m -> f (IxValue m)) -> m -> f m
+transitLookup k = at (TransitKeyword k) . _Just
 
-toMessagePack :: Maybe Event -> MP.Object
+makeNewUser :: [Transit] -> Maybe Event
+makeNewUser [TransitMap m] = NewUser <$>
+                              Just (m ^.. transitLookup "group-ids" . _TransitArray . each . _TransitUUID) <*>
+                              m ^? transitLookup "id" . _TransitUUID
+makeNewUser _ = Nothing
+
+makeNewAdmin :: [Transit] -> Maybe Event
+makeNewAdmin [TransitArray [TransitUUID groupId, TransitUUID adminId]] = Just $ NewAdmin groupId adminId
+makeNewAdmin _ = Nothing
+
+parseEvent :: Text -> [Transit] -> Maybe Event
+parseEvent k info = case k of
+                      "braid.client/new-user" -> makeNewUser info
+                      "braid.client/new-admin" -> makeNewAdmin info
+                      _ -> Nothing
+
+instance Transitable Event where
+  fromTransit eventInfo@(TransitArray ((TransitKeyword k):rest)) = case parseEvent k rest of
+                                                                     Just e -> e
+                                                                     Nothing -> Other eventInfo
+  fromTransit eventInfo = Other eventInfo
+
+  -- TODO: implement toTransit (although we don't need to send events)
+  toTransit (Other t) = t
+  toTransit _ = TransitNil
+
+{-
+braid.client/thread
+braid.client/init-data
+socket/connected
+braid.client/create-tag
+braid.client/joined-group
+braid.client/update-users
+braid.client/invitation-received
+braid.client/name-change
+braid.client/user-new-avatar
+braid.client/left-group
+braid.client/user-connected
+braid.client/user-disconnected
+braid.client/new-user
+braid.client/user-left
+braid.client/new-admin
+braid.client/tag-descrption-change
+braid.client/retract-tag
+braid.client/new-intro
+braid.client/group-new-avatar
+braid.client/publicity-changed
+braid.client/new-bot
+braid.client/retract-bot
+braid.client/edit-bot
+braid.client/notify-message
+braid.client/hide-thread
+braid.client/show-thread
+-}
+
+toMessagePack :: Event -> MP.Object
 toMessagePack = fromTransit . toTransit
 
-fromMessagePack :: MP.Object -> Maybe Event
+fromMessagePack :: MP.Object -> Event
 fromMessagePack = fromTransit . toTransit
 
-dumpEvent :: Maybe Event -> BS.ByteString
+dumpEvent :: Event -> BS.ByteString
 dumpEvent = encode . toMessagePack
 
 readEvent :: BL.ByteString -> Maybe Event
 readEvent bs = case decodeLazy bs of
                 Left _ -> Nothing
-                Right obj -> fromMessagePack obj
+                Right obj -> Just $ fromMessagePack obj
